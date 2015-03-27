8566c478f4c5dfe9f32682b74c1655bec48dfd11
yksz <user4git@gmail.com>
2015-03-27 12:15:45 +0900

    Add include/sclib.h

---
+++ include/sclib.h
@@ -0,0 +1,10 @@
+#ifndef SCLIB_H
+#define SCLIB_H
+
+#include "sclib/array.h"
+#include "sclib/list.h"
+#include "sclib/stack.h"
+#include "sclib/queue.h"
+#include "sclib/hashmap.h"
+
+#endif /* SCLIB_H */
